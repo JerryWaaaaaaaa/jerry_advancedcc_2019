cc6b12a4724def50b9d47aef299b2c6a4eeed22a
Jerry <zw1141@nyu.edu>
2019-04-14 15:43:33 -0400

    hw05

---
+++ hw_04_audio_data_process/hw_05_I_O/src/ofApp.h
@@ -0,0 +1,105 @@
+#pragma once
+
+#include "ofMain.h"
+#include "ofxGui.h"
+
+class ofApp : public ofBaseApp{
+
+	public:
+		void setup();
+		void update();
+		void draw();
+
+		void keyPressed(int key);
+		void keyReleased(int key);
+		void mouseMoved(int x, int y );
+		void mouseDragged(int x, int y, int button);
+		void mousePressed(int x, int y, int button);
+		void mouseReleased(int x, int y, int button);
+		void mouseEntered(int x, int y);
+		void mouseExited(int x, int y);
+		void windowResized(int w, int h);
+		void dragEvent(ofDragInfo dragInfo);
+		void gotMessage(ofMessage msg);
+        void generatePoster();
+        void displayGrid();
+        void displayImage();
+        void uploadImage();
+        void saveFrame();
+        int getIndex(int row, int col);
+    
+        int counter = 0;
+    
+        // one unit of gui width and height
+        int gWidth = 200;;
+        int gHeight = 20;
+    
+        ofFbo fbo;
+    
+        // poster width and height;
+        const int size1 = 610;
+        const int size2 = 460;
+        float pWidth;
+        float pHeight;
+        int rows;
+        int cols;
+        float moduleWidth;
+        float moduleHeight;
+        ofVec2f origin;
+        vector<ofVec2f> modules;
+    
+        //  load panel
+        ofxPanel gui;
+    
+        // direction of the poster
+        ofxToggle vertical;
+        ofxToggle horizontal;
+    
+        // scale of the poster
+        ofxFloatSlider scale;
+    
+        // load image
+        ofxButton loadImage;
+        bool showImg = true;
+    
+        // display image
+        ofxButton showImage;
+    
+        // image position
+        ofxIntSlider imageRow;
+        ofxIntSlider imageCol;
+        ofxIntSlider imageWidth;
+        ofxIntSlider imageHeight;
+    
+        // input field
+        ofxInputField<string> header;
+        ofxInputField<string> text;
+    
+        // text position
+        ofxIntSlider headerRow;
+        ofxIntSlider headerCol;
+        ofxIntSlider bodyRow;
+        ofxIntSlider bodyCol;
+    
+        // color picker
+        ofxColorSlider backgroundColor;
+        ofxColorSlider fontColor;
+    
+        // show grid button
+        ofxButton showGrid;
+        bool show = true;
+    
+        // save button
+        ofxButton save;
+    
+        // image
+        ofImage img;
+        ofImage poster;
+    
+        // fonts
+        ofTrueTypeFont HelveticaBold;
+        ofTrueTypeFont HelveticaLight;
+    
+    
+		
+};
